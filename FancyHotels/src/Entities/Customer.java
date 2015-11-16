69000d627666cc24e9dd9a829aef5151d6621d8f
mholmes35 <morganholmes@lawn-128-61-44-78.lawn.gatech.edu>
2015-11-15 20:33:54 -0500

    Created NB project, created packages, began creating entity classes

---
+++ FancyHotels/src/Entities/Customer.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+/**
+ *
+ * @author morganholmes
+ */
+public class Customer extends User {
+    private String email;
+    private String username;
+    private String password;
+    /**
+     *
+     * @param un
+     * @param pw
+     * @param em
+     */
+    public Customer(String un, String pw, String em) {
+        super(un, pw);
+        this.email = em;
+        this.username = un;
+        this.password = pw;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
---
+++ FancyHotels/src/Entities/User.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+/**
+ *
+ * @author morganholmes
+ */
+public class User {
+    private String username;
+    private String password;
+    public User(String un, String pw) {
+        this.username = un;
+        this.password = pw;
+    }
+    public String getUsername() {
+        return this.username;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
